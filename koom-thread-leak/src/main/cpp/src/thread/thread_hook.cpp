07606392b612748b4798fc4fb66c80a55a601b12
xueqiushi <xueqiushi@kuaishou.com>
2021-08-17 22:14:39 +0800

    Support fork dump for Android 11
    Signed-off-by: xueqiushi <xueqiushi@kuaishou.com>

--- koom-thread-leak/src/main/cpp/src/thread/thread_hook.cpp
+++ koom-thread-leak/src/main/cpp/src/thread/thread_hook.cpp
@@ -19,11 +19,11 @@
 
 #include "thread_hook.h"
 #include <dlopencb.h>
-#include <linux/prctl.h>
-#include <sys/prctl.h>
-#include <xhook.h>
-#include <syscall.h>
+#include <kwai_util/kwai_macros.h>
 #include <link.h>
+#include <sys/prctl.h>
+#include <syscall.h>
+#include <xhook.h>
 
 namespace koom {
 
@@ -55,8 +55,8 @@ void ThreadHooker::InitHook() {
   DlopenCb::GetInstance().AddCallback(DlopenCallback);
 }
 
-void ThreadHooker::DlopenCallback(std::set<std::string> &libs, int source, std::string
-&source_lib) {
+void ThreadHooker::DlopenCallback(std::set<std::string> &libs, int source,
+                                  std::string &source_lib) {
   HookLibs(libs, source);
 }
 
@@ -92,15 +92,11 @@ bool ThreadHooker::RegisterSo(const std::string &lib, int source) {
   return true;
 }
 
-int ThreadHooker::HookThreadCreate(pthread_t *tidp,
-                                   const pthread_attr_t *attr,
-                                   void *(*start_rtn)(
-                                       void *),
-                                   void *arg) {
+int ThreadHooker::HookThreadCreate(pthread_t *tidp, const pthread_attr_t *attr,
+                                   void *(*start_rtn)(void *), void *arg) {
   if (hookEnabled() && start_rtn != nullptr) {
     auto time = Util::CurrentTimeNs();
-    koom::Log::info(thread_tag,
-                    "HookThreadCreate");
+    koom::Log::info(thread_tag, "HookThreadCreate");
     auto *hook_arg = new StartRtnArg(arg, Util::CurrentTimeNs(), start_rtn);
     auto *thread_create_arg = hook_arg->thread_create_arg;
     void *thread = koom::CallStack::GetCurrentThread();
@@ -109,9 +105,7 @@ int ThreadHooker::HookThreadCreate(pthread_t *tidp,
     }
     koom::CallStack::FastUnwind(thread_create_arg->pc, koom::Constant::kMaxCallStackDepth);
     thread_create_arg->stack_time = Util::CurrentTimeNs() - time;
-    return pthread_create(tidp,
-                          attr,
-                          reinterpret_cast<void *(*)(void *)>(HookThreadStart),
+    return pthread_create(tidp, attr, reinterpret_cast<void *(*)(void *)>(HookThreadStart),
                           reinterpret_cast<void *>(hook_arg));
   }
   return pthread_create(tidp, attr, start_rtn, arg);
@@ -119,19 +113,17 @@ int ThreadHooker::HookThreadCreate(pthread_t *tidp,
 
 ALWAYS_INLINE void ThreadHooker::HookThreadStart(void *arg) {
   koom::Log::info(thread_tag, "HookThreadStart");
-  auto *hookArg = (StartRtnArg *) arg;
+  auto *hookArg = (StartRtnArg *)arg;
   pthread_attr_t attr;
   pthread_t self = pthread_self();
   int state = 0;
   if (pthread_getattr_np(self, &attr) == 0) {
     pthread_attr_getdetachstate(&attr, &state);
   }
-  int tid = (int) syscall(SYS_gettid);
-
-  koom::Log::info(thread_tag, "HookThreadStart %p, %d, %d", self, tid,
-                  hookArg->thread_create_arg->stack_time);
-  auto info = new HookAddInfo(tid, Util::CurrentTimeNs(), self,
-                              state == PTHREAD_CREATE_DETACHED, hookArg->thread_create_arg);
+  int tid = (int)syscall(SYS_gettid);
+  koom::Log::info(thread_tag, "HookThreadStart %p, %d, %d", self, tid, hookArg->thread_create_arg->stack_time);
+  auto info = new HookAddInfo(tid, Util::CurrentTimeNs(), self, state == PTHREAD_CREATE_DETACHED,
+                              hookArg->thread_create_arg);
 
   sHookLooper->post(ACTION_ADD_THREAD, info);
   void *(*start_rtn)(void *) = hookArg->start_rtn;
@@ -141,24 +133,23 @@ ALWAYS_INLINE void ThreadHooker::HookThreadStart(void *arg) {
 }
 
 int ThreadHooker::HookThreadDetach(pthread_t t) {
-  if (!hookEnabled()) return pthread_detach(t);
+  if (!hookEnabled())
+    return pthread_detach(t);
 
-  int c_tid = (int) syscall(SYS_gettid);
-  koom::Log::info(thread_tag, "HookThreadDetach c_tid:%0x",
-                  c_tid);
+  int c_tid = (int)syscall(SYS_gettid);
+  koom::Log::info(thread_tag, "HookThreadDetach c_tid:%0x", c_tid);
 
   auto info = new HookInfo(t, Util::CurrentTimeNs());
   sHookLooper->post(ACTION_DETACH_THREAD, info);
   return pthread_detach(t);
 }
 
-int ThreadHooker::HookThreadJoin(pthread_t t,
-                                 void **return_value) {
-  if (!hookEnabled()) return pthread_join(t, return_value);
+int ThreadHooker::HookThreadJoin(pthread_t t, void **return_value) {
+  if (!hookEnabled())
+    return pthread_join(t, return_value);
 
-  int c_tid = (int) syscall(SYS_gettid);
-  koom::Log::info(thread_tag, "HookThreadJoin c_tid:%0x",
-                  c_tid);
+  int c_tid = (int)syscall(SYS_gettid);
+  koom::Log::info(thread_tag, "HookThreadJoin c_tid:%0x", c_tid);
 
   auto info = new HookInfo(t, Util::CurrentTimeNs());
   sHookLooper->post(ACTION_JOIN_THREAD, info);
@@ -166,10 +157,11 @@ int ThreadHooker::HookThreadJoin(pthread_t t,
 }
 
 void ThreadHooker::HookThreadExit(void *return_value) {
-  if (!hookEnabled()) pthread_exit(return_value);
+  if (!hookEnabled())
+    pthread_exit(return_value);
 
   koom::Log::info(thread_tag, "HookThreadExit");
-  int tid = (int) syscall(SYS_gettid);
+  int tid = (int)syscall(SYS_gettid);
   char thread_name[16]{};
   prctl(PR_GET_NAME, thread_name);
   auto info = new HookExitInfo(pthread_self(), tid, thread_name, Util::CurrentTimeNs());
@@ -177,13 +169,9 @@ void ThreadHooker::HookThreadExit(void *return_value) {
   pthread_exit(return_value);
 }
 
-void ThreadHooker::Start() {
-  ThreadHooker::InitHook();
-}
+void ThreadHooker::Start() { ThreadHooker::InitHook(); }
 
 void ThreadHooker::Stop() {}
 
-bool ThreadHooker::hookEnabled() {
-  return isRunning;
-}
-}
+bool ThreadHooker::hookEnabled() { return isRunning; }
+} // namespace koom
